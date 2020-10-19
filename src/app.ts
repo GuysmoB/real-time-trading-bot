7eeaca811d9f1efb5389ed83856aa954bff64c82
rbrauge <rbrauge@.asi.fr>
2020-10-19 17:31:28 +0200

    save

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+// https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript
+
+class App {
+
+    constructor() {
+        console.log('hello world')
+    }
+}
+
+new App();
