09c5396ca4a154f6dda15bfce952388454d564c1
rbrauge <rbrauge@.asi.fr>
2020-10-22 16:55:19 +0200

    save

--- src/services/utils-service.ts
+++ src/services/utils-service.ts
@@ -1,46 +1,47 @@
-import fs from 'fs';
+import fs from "fs";
 import { promisify } from "util";
+import ig from "node-ig-api";
 
 export class UtilsService {
-
-    constructor () { }
+  constructor() {}
 
   /**
    * Parse et push les donnees CSV.
    */
   async getDataFromFile(): Promise<any> {
     const result = [];
-    const content = await promisify(fs.readFile)('src\\assets\\EURUSD60.csv', 'UTF-8');
-    const csvToRowArray = content.split('\r\n');
-      for (let index = 1; index < csvToRowArray.length - 1; index++) {
-        const element = csvToRowArray[index].split('\t'); // d, o, h, l, c, v
-        result.push({
-          date: element[0],
-          open: parseFloat(element[1]),
-          high: parseFloat(element[2]),
-          low: parseFloat(element[3]),
-          close: parseFloat(element[4]),
-          volume: parseFloat(element[5])
-        });
-      }
-      return result;
+    const content = await promisify(fs.readFile)("src\\assets\\EURUSD60.csv", "UTF-8");
+    const csvToRowArray = content.split("\r\n");
+    for (let index = 1; index < csvToRowArray.length - 1; index++) {
+      const element = csvToRowArray[index].split("\t"); // d, o, h, l, c, v
+      result.push({
+        date: element[0],
+        open: parseFloat(element[1]),
+        high: parseFloat(element[2]),
+        low: parseFloat(element[3]),
+        close: parseFloat(element[4]),
+        volume: parseFloat(element[5]),
+      });
+    }
+    return result;
   }
 
   /**
    * Parse les données depuis IG.
    */
-  async parseData(ig: any, resolution: string, max: number) { 
+  async parseData(resolution: string, max: number) {
     const result = [];
-    const res = await ig.get('prices/CS.D.EURGBP.CFD.IP?resolution=' +resolution +'&max=' +max +'&pageSize=0', 3);
-    for (let i = 0; i < res.prices.length; i++) {
+    //const res = await nodeIg.histPrc('CC.D.LCO.USS.IP', 'HOUR_4', '2012-03-01T00:00:00', '2012-03-01T20:00:00');
+    const res = await ig.get("/prices/CS.D.EURGBP.CFD.IP?resolution=" + resolution + "&max=" + max + "&pageSize=0", 3);
+    console.log(res.body);
+    /* for (let i = 0; i < res.prices.length; i++) {
       result.push({ open: res.prices[i].openPrice.bid, 
                         close: res.prices[i].closePrice.bid,
                         high: res.prices[i].highPrice.bid,
                         low: res.prices[i].lowPrice.bid});  
-    }  
+    }   */
     return result;
   }
-
 
   /**
    * Permet de retourner le R:R
@@ -49,8 +50,7 @@ export class UtilsService {
     return this.round((closedPrice - entryPrice) / (entryPrice - initialStopLoss), 2);
   }
 
-
-    /**
+  /**
    * Retourne la valeur maximale en fonction de la source et de lookback
    */
   highest(data: any, index: number, source: string, lookback: number): number {
@@ -67,7 +67,6 @@ export class UtilsService {
     }
     return max;
   }
-
 
   /**
    * Retourne la valeur minimale en fonction de la source et de lookback
@@ -87,7 +86,6 @@ export class UtilsService {
     return min;
   }
 
-
   /**
    * Arrondi un nombre avec une certaine précision.
    */
@@ -96,8 +94,7 @@ export class UtilsService {
     return Math.round(value * multiplier) / multiplier;
   }
 
-
-    /**
+  /**
    * Retourne l'équivalent HeikenAshi
    */
   setHeikenAshiData(source: any): any {
@@ -109,7 +106,7 @@ export class UtilsService {
           close: this.round((source[j].open + source[j].high + source[j].low + source[j].close) / 4, 5),
           open: this.round((source[j].open + source[j].close) / 2, 5),
           low: source[j].low,
-          high: source[j].high
+          high: source[j].high,
         });
       } else {
         const haCloseVar = (source[j].open + source[j].high + source[j].low + source[j].close) / 4;
@@ -118,14 +115,13 @@ export class UtilsService {
           close: this.round(haCloseVar, 5),
           open: this.round(haOpenVar, 5),
           low: this.round(Math.min(source[j].low, Math.max(haOpenVar, haCloseVar)), 5),
-          high: this.round(Math.max(source[j].high, Math.max(haOpenVar, haCloseVar)), 5)
+          high: this.round(Math.max(source[j].high, Math.max(haOpenVar, haCloseVar)), 5),
         });
       }
     }
     return result;
   }
 
-  
   sma(data: any, index: number, periode: number): number {
     const result = [];
     const dataStart = index - periode;
@@ -138,8 +134,63 @@ export class UtilsService {
     } else {
       return 0;
     }
-    
   }
 }
 
 export default new UtilsService();
+
+/*
+minutesProcessed = [];
+minutesCandlesticks = [];
+currentTick = none
+previousTick = none
+
+streamData(data => {
+  previousTick = currentTick;
+  currentTick = data;
+  printf('Received tick', currentTick.time, currentTick.price);
+  tcikDatetimeObject = dateutils.parse(currentTick.time)
+  tick_dt = tickDatetimeObject.strftime('%m/%d/%Y %H:%M')
+  print(tick_dt, tcikDatetimeObject.minute);
+
+  if (not tick_dt in minutesProcessed) {}
+    print('starting new candlestick')
+    minutesProcessed.tick_dt = true
+    print(minutesProcessed).
+
+
+    if len(minuteCandlestick) > 0 {
+      minuteCandlestick[-1].close = previousTick.price
+    }
+
+    minuteCandlesticks.append({
+      minute: tick_dt,
+      open: currentTick.price,
+      high: currentTick.price,
+      low: currentTick.price,
+    })
+  }
+
+  if (len(minuteCandlestick)) > 0 {
+    currentCandlestick = minuteCandletick[-1]
+    if (currentTick.price > currentCandlestick.high) {
+      currentCandlestick.high = currentTick.price; 
+    }
+    if (currentTick.price < currentCandlestick.low) {
+      currentCandlestick.low = currentTick.price; 
+    }
+
+    printf('Candlestick')
+    for (candlestick in minuteCandlestick) {
+      print(candlestick)
+    }
+  }
+
+
+
+
+
+
+})
+
+ */
