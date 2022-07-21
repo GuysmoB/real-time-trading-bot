8b2186c820d446e0ba9d3d0633a5f8cd88995d27
Remi <42407006+GuysmoB@users.noreply.github.com>
2022-07-21 09:48:20 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,9 @@
 export class Config {
 
-    token = '1394125992:AAGNZ30GK2HePdaX3rmsJIRcosXpn2neU-k';
-    chatId = '-451985401';
+    token = '';
+    chatId = '';
     firebaseConfig = {
-        apiKey: "AIzaSyAn1Jn7J-vumyC-XL1LY7AJEq7R_oqUGdc",
-        authDomain: "real-time-trading-bot.firebaseapp.com",
-        databaseURL: "https://real-time-trading-bot.firebaseio.com",
-        projectId: "real-time-trading-bot",
-        storageBucket: "real-time-trading-bot.appspot.com",
-        messagingSenderId: "711877653368",
-        appId: "1:711877653368:web:5c5e1ebab700ab23624e8d"
+        
     };
 
     constructor() { }
